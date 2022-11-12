40d6dd92e765dd4f9b8f878b688575681c3f8e6b
Andrew Hill <andrew@textile.io>
2022-11-12 08:58:44 -0800

    fix(outputs): format for action outputs
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- github-reporting-tool.py
+++ github-reporting-tool.py
@@ -14,37 +14,59 @@ if os.environ.get('INPUT_ORG') is None:
     print('!!! missing INPUT_ORG environment variable !!!')
 
 
+
 #Get list of repos
-print("Repo List:")
-repos = org.get_repos()
-for r in repos:
-    print("  ",r.git_url)
-    
+try: 
+    repos = org.get_repos()
+    repo_lines = "Repos\n"
+    for r in repos:
+        repo_lines += "  " + r.git_url + "\n"
+    print(f"::set-output name=repos::{repo_lines}")
+except:
+    message = "failed to list repos\n"
+    print(f"::set-output name=repos::{message}")
+
 
 #Get list of teams
-print("\nTeam List:")
-teams = org.get_teams()
-for t in teams:
-    print("   ",t.name)
-    team_repos = t.get_repos()
-    for repos in team_repos:
-        print("     ",repos.git_url)
+try:
+    teams = org.get_teams()
+    team_lines = "\nTeams\n"
+    for t in teams:
+        team_lines += "  " + t.name + "\n"
+        team_repos = t.get_repos()
+        for r in team_repos:
+            team_lines += "    " + r.git_url + "\n"
+    print(f"::set-output name=teams::{team_lines}")
+except:
+    message = "failed to list teams\n"
+    print(f"::set-output name=teams::{message}")
 
 #Get list of team members
-print("\nTeam Membership List:")
-teams = org.get_teams()
-for t in teams:
-    print("  ",t.name," Team Members:")
-    for m in t.get_members():
-        #print("      Name: ",m.name,", Email: ",m.email,", ID: ",m.id,", Login: ",m.login)
-        print("      ",m.login)
+try:
+    membership_lines = "\nMembers\n"
+    teams = org.get_teams()
+    for t in teams:
+        membership_lines += "  " + t.name + " Team Members:\n"
+        print("  ",t.name," Team Members:")
+        for m in t.get_members():
+            #print("      Name: ",m.name,", Email: ",m.email,", ID: ",m.id,", Login: ",m.login)
+            membership_lines += "    " + m.login + "\n"
+    print(f"::set-output name=members::{membership_lines}")
+except:
+    message = "failed to list members\n"
+    print(f"::set-output name=members::{message}")
 
 #Get list of repos
-print("\nDirect Repo Rights:")
-repos = org.get_repos()
-for r in repos:
-    print("  ",r.git_url)
-    collaborators = r.get_collaborators()
-    for c in collaborators:
-        #print("      Name: ",c.name,", Email: ",c.email,", ID: ",c.id,", Login: ",c.login)
-        print("      ",c.login)
+try: 
+    rights_lines = "\nRights\n"
+    repos = org.get_repos()
+    for r in repos:
+        rights_lines += "  " + r.git_url + "\n"
+        collaborators = r.get_collaborators()
+        for c in collaborators:
+            #print("      Name: ",c.name,", Email: ",c.email,", ID: ",c.id,", Login: ",c.login)
+            rights_lines += "  " + c.login + "\n"
+    print(f"::set-output name=rights::{rights_lines}")
+except:
+    message = "failed to list rights\n"
+    print(f"::set-output name=rights::{message}")
