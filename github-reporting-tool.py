e9a26f049f55abfcb34cde11553c8deaa58877d9
Andrew Hill <andrew@textile.io>
2022-11-12 14:10:54 -0800

    fix(outputs): try diff org
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- github-reporting-tool.py
+++ github-reporting-tool.py
@@ -96,10 +96,14 @@ audit["errors"] = errors
 
 
 pth = os.environ['INPUT_PATH']
+print(pth)
 dr = os.path.split(pth)
 if not os.path.exists(dr[0]):
+    print("making pth")
+    print(dr[0])
     os.makedirs(dr[0], exist_ok=True)
 
 json_object = json.dumps(audit, indent=4)
 with open(pth, "w+") as outfile:
+    print("writing")
     outfile.write(json_object)
