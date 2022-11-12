6b6da27f70496041de924db8b1a523c3ac94aad9
Andrew Hill <andrew@textile.io>
2022-11-12 11:02:42 -0800

    fix(outputs): try diff org
    Signed-off-by: Andrew Hill <andrew@textile.io>

--- github-reporting-tool.py
+++ github-reporting-tool.py
@@ -20,23 +20,22 @@ print(f"::set-output name=org::{org}")
 
 errors = {}
 #Get list of repos
+repo_lines = []
 try: 
     repos = org.get_repos()
-    repo_lines = []
     # repo_lines = "Repos\n"
     for r in repos:
         # repo_lines += "  " + r.git_url + "\n"
         repo_lines.append(r.git_url)
-    print(f"::set-output name=repos::{repo_lines}")
 except:
     message = "[]"
     print(f"::set-output name=repos::{message}")
-
+print(f"::set-output name=repos::{repo_lines}")
 
 #Get list of teams
+team_lines = {}
 try:
     teams = org.get_teams()
-    team_lines = {}
     for t in teams:
         # team_lines += "  " +  + "\n"
         team_lines[t.name] = []
@@ -44,28 +43,28 @@ try:
         for r in team_repos:
             team_lines[t.name].append(r.git_url)
             # team_lines += "    " + r.git_url + "\n"
-    print(f"::set-output name=teams::{json.dumps(team_lines)}")
 except:
     message = "failed to list teams"
     errors["teams"] = message
+print(f"::set-output name=teams::{json.dumps(team_lines)}")
 
 #Get list of team members
+membership_lines = {}
 try:
-    membership_lines = {}
     teams = org.get_teams()
     for t in teams:
         membership_lines[t.name] = []
         for m in t.get_members():
             #print("      Name: ",m.name,", Email: ",m.email,", ID: ",m.id,", Login: ",m.login)
             membership_lines[t.name].append(m.login)
-    print(f"::set-output name=members::{json.dumps(membership_lines)}")
 except:
     message = "failed to list members"
     errors["members"] = message
+print(f"::set-output name=members::{json.dumps(membership_lines)}")
 
 #Get list of repos
+rights_lines = {}
 try: 
-    rights_lines = {}
     repos = org.get_repos()
     for r in repos:
         rights_lines[r.git_url] = []
@@ -73,10 +72,10 @@ try:
         for c in collaborators:
             rights_lines[r.git_url].append(c.login)
             #print("      Name: ",c.name,", Email: ",c.email,", ID: ",c.id,", Login: ",c.login)
-    print(f"::set-output name=rights::{json.dumps(rights_lines)}")
 except:
     message = "failed to list rights"
     errors["rights"] = message
-
+    
+print(f"::set-output name=rights::{json.dumps(rights_lines)}")
 
 print(f"::set-output name=errors::{json.dumps(errors)}")
